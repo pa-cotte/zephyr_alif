44e380b6e18ca7ddf5b3dd4f4f18330530302717
Hemanth Kumar <hemanth@alifsemi.com>
2024-04-02 23:51:27 +0530

    boards: arm: add alif_e7_dk_rtss_hp and alif_e7_dk_rtss_he boards
    Added initial support for RTSS-HE and RTSS-HP boards of
    Alif Ensemble E7 family of devices.
    
    Signed-off-by: Hemanth Kumar <hemanth@alifsemi.com>
    Signed-off-by: Jagadeesh Krishnanjanappa <jagadeesh@alifsemi.com>

---
+++ soc/arm/alif_ensemble/e7/soc_e7_dk_rtss_hp.c
@@ -0,0 +1,53 @@
+/* Copyright (c) 2024 Alif Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/cpu.h>
+#include <zephyr/drivers/gpio/gpio_mmio32.h>
+#include <zephyr/init.h>
+#include <soc.h>
+#include <zephyr/linker/linker-defs.h>
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * @return 0
+ */
+static int ensemble_e7_dk_rtss_hp_init(void)
+{
+	unsigned int data;
+
+	/* Might need to move later.. Just putting this here for now..*/
+	/* Enable UART clock and clock selection bits in CFGMST0 */
+	sys_write32(0xFFFF, 0x4902F008);
+	/* Set PLL clock of 160 MHz */
+	sys_write32(0x10000, 0x4903F00C);
+
+	/* CGU_UART_CLK source to PLL */
+	data = sys_read32(0x1A602008);
+	data |= 1U << 8;
+	sys_write32(data, 0x1A602008);
+
+	/* Enable CGU_UART_CLK */
+	data = sys_read32(0x1A602014);
+	data |= 1U << 17;
+	sys_write32(data, 0x1A602014);
+
+	/* Switch HOSTUARTCLK Source to CGU_UART_CLK */
+	sys_write32(2, 0x1A010850);
+
+
+	/* Setting expansion master0 control register value */
+	/* for enabling clock */
+	data |= 0xc0000000;
+	sys_write32(data, 0x4902F000);
+
+	data = sys_read32(0x43007010);
+	data |= (1 << 16);
+	sys_write32(data, 0x43007010);
+
+	return 0;
+}
+
+SYS_INIT(ensemble_e7_dk_rtss_hp_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
