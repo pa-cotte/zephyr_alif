2af14af25afb0ba45e697b12c3e84101fbda4bc5
Aravind Krishnan <aravind.krishnan@alifsemi.com>
2024-05-28 19:36:09 +0530

    samples:drivers:counter:alarm Add Designware RTC node lable in alarm application
    This update introduces DesignWare RTC driver nodes to the alarm sample application.
    
    Signed-off-by: Aravind Krishnan <aravind.krishnan@alifsemi.com>

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -51,6 +51,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define TIMER DT_NODELABEL(counter0)
 #elif defined(CONFIG_COUNTER_SNPS_DW)
 #define TIMER DT_NODELABEL(timer0)
+#elif defined(CONFIG_COUNTER_RTC_SNPS_DW)
+#define TIMER DT_NODELABEL(rtc0)
 #elif defined(CONFIG_COUNTER_TIMER_RPI_PICO)
 #define TIMER DT_NODELABEL(timer)
 #endif
