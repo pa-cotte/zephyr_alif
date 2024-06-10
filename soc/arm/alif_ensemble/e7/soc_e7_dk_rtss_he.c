56472a50a7a8e68cf48e4446e43e5c73c4a55c5d
Ganesh Kumar R <ganesh.ramani@alifsemi.com>
2024-06-10 10:28:17 +0530

    soc: arm: alif_ensemble: Enable SPI clock for DesignWare SPI.
    This update enables the Designware SPI clock in the Alif SoC.
    
    Signed-off-by: Ganesh Kumar R <ganesh.ramani@alifsemi.com>

--- soc/arm/alif_ensemble/e7/soc_e7_dk_rtss_he.c
+++ soc/arm/alif_ensemble/e7/soc_e7_dk_rtss_he.c
@@ -43,6 +43,22 @@ static int ensemble_e7_dk_rtss_he_init(void)
 	data |= 0xc0000000;
 	sys_write32(data, 0x4902F000);
 
+	/*
+	 * Setting expansion master0 SPI control register values
+	 * 0xf at 8-11 bit is setting all 4 SPI instances as master
+	 * bit 0-3; ss_in_sel; 0 - from io pad; 1 - from ssi_in_val
+	 * bit 8-11; ss_in_val; when ss_in_sel=1, feed ss_in_val to SSI,
+	 * each bit controls one SSI instance.
+	 * For setting an spi instance as slave, put 0 in the corresponding
+	 * bit position of both 8-11 and 0-3 bit fields.
+	 * For example if we want to set SPI1 as master and
+	 * remaining instances as slave, set the 1st bit for ss_in_sel, which will
+	 * make ss_in_val to feed to SSI, and set the corresponding ss_in_val bit.
+	 * here for SPI1 as master set the 9th bit. So the value to feed SPI1 as
+	 * master and remaining as slave is 0x0202.
+	 */
+	sys_write32(0x0202, 0x4902F028);
+
 	data = sys_read32(0x43007010);
 	data |= (1 << 16);
 	sys_write32(data, 0x43007010);
