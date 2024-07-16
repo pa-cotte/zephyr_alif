7936ee81f0c076978a746f833d95049efbfe6d5a
Tanay Rami <tanay@alifsemi.com>
2024-07-16 16:21:21 +0530

    boards: arm: add Ensemble alif_e1c_dk_rtss_he
    Added initial support for Alif Ensemble E1C DevKit.
    
    Signed-off-by: Tanay Rami <tanay@alifsemi.com>

---
+++ soc/arm/alif_ensemble/e1c/soc_e1c_dk_rtss_he.c
@@ -0,0 +1,56 @@
+/* Copyright (c) 2024 Alif Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/cpu.h>
+#include <zephyr/drivers/gpio/gpio_mmio32.h>
+#include <zephyr/init.h>
+#include <soc.h>
+#include <zephyr/linker/linker-defs.h>
+#ifdef CONFIG_REBOOT
+#include <zephyr/sys/reboot.h>
+#include <se_service.h>
+#endif
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * @return 0
+ */
+static int ensemble_e1c_dk_rtss_he_init(void)
+{
+	/* enable all UART[5-0] modules and
+	 * select UART[5-0]_SCLK as SYST_PCLK clock. */
+	sys_write32(0xFFFF, 0x4902F008);
+
+	/* LPUART settings */
+	if (IS_ENABLED(CONFIG_SERIAL)) {
+		/* Enable clock supply for LPUART */
+		sys_write32(0x1, AON_RTSS_HE_LPUART_CKEN);
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_REBOOT
+void sys_arch_reboot(int type)
+{
+	switch (type) {
+	case SYS_REBOOT_WARM:
+		/* Use Cold boot until NVIC reset is fully working */
+		/* se_service_boot_reset_cpu(EXTSYS_1); */
+		se_service_boot_reset_soc();
+		break;
+	case SYS_REBOOT_COLD:
+		se_service_boot_reset_soc();
+		break;
+
+	default:
+		/* Do nothing */
+		break;
+	}
+}
+#endif
+
+SYS_INIT(ensemble_e1c_dk_rtss_he_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
