a499afd3ee11b1e1063f5cf6acb896b70b6fa622
Tanay Rami <tanay@alifsemi.com>
2024-10-15 20:01:11 +0530

    dts: e1c: add support for DMA2 node
    adding DMA2 node to e1c soc.
    
    Signed-off-by: Tanay Rami <tanay@alifsemi.com>

--- soc/arm/alif_ensemble/e1c/soc_e1c_dk_rtss_he.c
+++ soc/arm/alif_ensemble/e1c/soc_e1c_dk_rtss_he.c
@@ -137,6 +137,16 @@ static int ensemble_e1c_dk_rtss_he_init(void)
 		alif_tgu_setup();
 	}
 
+	/* Enable DMA */
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma2), arm_dma_pl330, okay)
+	sys_set_bits(M55HE_CFG_HE_CLK_ENA, BIT(4));
+	sys_write32(0x1111, EVTRTRLOCAL_DMA_REQ_CTRL);
+	sys_clear_bits(M55HE_CFG_HE_DMA_CTRL, BIT(0));
+	sys_write32(0U, M55HE_CFG_HE_DMA_IRQ);
+	sys_write32(0U, M55HE_CFG_HE_DMA_PERIPH);
+	sys_set_bits(M55HE_CFG_HE_DMA_CTRL, BIT(16));
+#endif
+
 	return 0;
 }
 
