ba4c8bcf3b45dd0aa7da8d2a362fa72bd594457e
Ganesh Kumar R <ganesh.ramani@alifsemi.com>
2024-11-12 14:08:20 +0530

    soc: alif : B1C SPI Support
    Adding SPI Support for Balletto
    
    Signed-off-by: Ganesh Kumar R <ganesh.ramani@alifsemi.com>

--- soc/arm/alif_balletto/b1/soc_b1_dk_rtss_he.c
+++ soc/arm/alif_balletto/b1/soc_b1_dk_rtss_he.c
@@ -90,6 +90,36 @@ static int balletto_b1_dk_rtss_he_init(void)
 		sys_write32(0x1, EXPSLV_OSPI_CTRL);
 	}
 
+	if (IS_ENABLED(CONFIG_SPI_DW)) {
+		/*
+		 * Setting expansion master0 SPI control register values
+		 * 0xf at 8-11 bit is setting all 4 SPI instances as master
+		 * bit 0-3; ss_in_sel; 0 - from io pad; 1 - from ssi_in_val
+		 * bit 8-11; ss_in_val; when ss_in_sel=1, feed ss_in_val to SSI,
+		 * each bit controls one SSI instance.
+		 * For setting an spi instance as slave, put 0 in the corresponding
+		 * bit position of both 8-11 and 0-3 bit fields.
+		 * For example if we want to set SPI1 as master and
+		 * remaining instances as slave, set the 1st bit for ss_in_sel, which will
+		 * make ss_in_val to feed to SSI, and set the corresponding ss_in_val bit.
+		 * here for SPI1 as master set the 9th bit. So the value to feed SPI1 as
+		 * master and remaining as slave is 0x0202.
+		 */
+		sys_write32(0x0202, SSI_CTRL_EN);
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(lpspi0), okay)
+		/*Clock : LP-SPI*/
+		sys_set_bit(HE_PER_CLK_EN, BIT(16));
+
+		/* LP-SPI0 Mode Selection */
+		/* To Slave Set Bit : 15  */
+		/* To Master Clear Bit : 15 */
+		sys_clear_bit(HE_PER_CLK_EN, BIT(15));
+
+		/*LP-SPI0 Flex GPIO*/
+		sys_write32(0x1, VBAT_GPIO_CTRL_EN);
+#endif
+	}
 	return 0;
 }
 
