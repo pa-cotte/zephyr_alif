4cf637446f628a6c72818f03d31220df30310ded
Manoj A Murudi <manoj.murudi@alifsemi.com>
2025-03-12 12:04:50 +0530

    drivers: pwm: Use UTIMER LL driver and channel in PWM driver
    Updated the PWM driver to use the UTIMER LL driver for register access.
    Replaced 'driver-type' usage with a channel parameter for dynamic
    selection.
    
    Signed-off-by: Manoj A Murudi <manoj.murudi@alifsemi.com>

--- drivers/pwm/pwm_alif_utimer.c
+++ drivers/pwm/pwm_alif_utimer.c
@@ -9,28 +9,32 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/pinctrl.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/logging/log.h>
 
-#include <utimer.h>
+#include "utimer.h"
 #include <zephyr/dt-bindings/timer/alif_utimer.h>
 
-#include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pwm_alif_utimer, CONFIG_PWM_LOG_LEVEL);
+
+#define NUM_CHANNELS  2U
 
 /** PWM configuration */
 struct pwm_alif_utimer_config {
 	uint32_t global_base;
 	uint32_t timer_base;
-	uint32_t frequency;
 	uint32_t counterdirection;
-	uint32_t driver_type;
-	const uint8_t timer;
+	const uint8_t timer_id;
 	const struct pinctrl_dev_config *pcfg;
+	const struct device *clk_dev;
+	clock_control_subsys_t clkid;
 };
 
 /** PWM runtime data configuration */
 struct pwm_alif_utimer_data {
 	uint32_t prev_period;
 	uint32_t prev_pulse;
+	uint32_t frequency;
 	pwm_flags_t prev_flags;
 };
 
@@ -38,93 +42,42 @@ static void utimer_set_direction(uint32_t reg_base, uint8_t direction)
 {
 	switch (direction) {
 	case ALIF_UTIMER_COUNTER_DIRECTION_UP:
-		sys_clear_bit(UTIMER_CNTR_CTRL(reg_base), CNTR_CTRL_DIR_DOWN_BIT);
+		alif_utimer_set_up_counter(reg_base);
 		break;
 	case ALIF_UTIMER_COUNTER_DIRECTION_DOWN:
-		sys_set_bit(UTIMER_CNTR_CTRL(reg_base), CNTR_CTRL_DIR_DOWN_BIT);
+		alif_utimer_set_down_counter(reg_base);
 		break;
 	case ALIF_UTIMER_COUNTER_DIRECTION_TRIANGLE:
-		sys_write32(CNTR_CTRL_TRIANGLE_BUF_TROUGH_CREST,
-				UTIMER_CNTR_CTRL(reg_base));
+		alif_utimer_set_triangular_counter(reg_base);
 		break;
 	default:
 		LOG_ERR("invalid counter-direction");
 	}
 }
 
-static void utimer_config_driver(uint32_t reg_base, uint8_t driver, uint32_t value)
+static uint32_t utimer_get_driver_config(uint8_t direction, pwm_flags_t flags)
 {
-	uint32_t temp;
-	uint32_t mask = (COMPARE_CTRL_DRV_COMP_MATCH_Msk | COMPARE_CTRL_DRV_CYCLE_END_Msk);
+	uint32_t config_val;
 
-	if (driver & ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) {
-		temp = sys_read32(UTIMER_COMPARE_CTRL_A(reg_base));
-		temp &= ~mask;
-		temp |= value;
-		sys_write32(temp, UTIMER_COMPARE_CTRL_A(reg_base));
+	switch (direction) {
+	case ALIF_UTIMER_COUNTER_DIRECTION_UP:
+	case ALIF_UTIMER_COUNTER_DIRECTION_DOWN:
+		config_val = (flags & PWM_POLARITY_INVERTED) ?
+			(COMPARE_CTRL_DRV_HIGH_AT_COMP_MATCH | COMPARE_CTRL_DRV_LOW_AT_CYCLE_END) :
+			(COMPARE_CTRL_DRV_LOW_AT_COMP_MATCH | COMPARE_CTRL_DRV_HIGH_AT_CYCLE_END);
+		break;
+	case ALIF_UTIMER_COUNTER_DIRECTION_TRIANGLE:
+		config_val = (flags & PWM_POLARITY_INVERTED) ?
+		(COMPARE_CTRL_DRV_TOGGLE_AT_COMP_MATCH | COMPARE_CTRL_DRV_RETAIN_VAL_AT_CYCLE_END |
+					COMPARE_CTRL_DRV_START_VAL_HIGH) :
+		(COMPARE_CTRL_DRV_TOGGLE_AT_COMP_MATCH | COMPARE_CTRL_DRV_RETAIN_VAL_AT_CYCLE_END |
+					COMPARE_CTRL_DRV_START_VAL_LOW);
+		break;
+	default:
+		config_val = 0;
+		LOG_ERR("invalid counter-direction");
 	}
-	if (driver & ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE) {
-		temp = sys_read32(UTIMER_COMPARE_CTRL_B(reg_base));
-		temp &= ~mask;
-		temp |= value;
-		sys_write32(temp, UTIMER_COMPARE_CTRL_B(reg_base));
-	}
-}
-
-static void utimer_enable_output(uint32_t reg_base, uint8_t driver, uint8_t timer)
-{
-	if (driver & ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) {
-		sys_clear_bit(UTIMER_GLB_DRIVER_OEN(reg_base), (timer * 2));
-	}
-	if (driver & ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE) {
-		sys_clear_bit(UTIMER_GLB_DRIVER_OEN(reg_base), ((timer * 2) + 1));
-	}
-}
-
-static void utimer_compare_disable_driver(uint32_t reg_base, uint8_t driver, pwm_flags_t pwm_flag)
-{
-	if (driver & ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) {
-		sys_clear_bit(UTIMER_COMPARE_CTRL_A(reg_base),
-				COMPARE_CTRL_DRV_DRIVER_EN_BIT);
-		if (pwm_flag == PWM_POLARITY_INVERTED) {
-			sys_set_bit(UTIMER_COMPARE_CTRL_A(reg_base),
-					COMPARE_CTRL_DRV_DISABLE_VAL_HIGH_BIT);
-		}
-	}
-	if (driver & ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE) {
-		sys_clear_bit(UTIMER_COMPARE_CTRL_B(reg_base),
-				COMPARE_CTRL_DRV_DRIVER_EN_BIT);
-		if (pwm_flag == PWM_POLARITY_INVERTED) {
-			sys_set_bit(UTIMER_COMPARE_CTRL_B(reg_base),
-					COMPARE_CTRL_DRV_DISABLE_VAL_HIGH_BIT);
-		}
-	}
-}
-
-static void utimer_compare_enable_driver(uint32_t reg_base, uint8_t driver)
-{
-	if (driver & ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) {
-		sys_set_bit(UTIMER_COMPARE_CTRL_A(reg_base),
-				COMPARE_CTRL_DRV_DRIVER_EN_BIT);
-			sys_clear_bit(UTIMER_COMPARE_CTRL_A(reg_base),
-					COMPARE_CTRL_DRV_DISABLE_VAL_HIGH_BIT);
-	}
-	if (driver & ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE) {
-		sys_set_bit(UTIMER_COMPARE_CTRL_B(reg_base),
-				COMPARE_CTRL_DRV_DRIVER_EN_BIT);
-			sys_clear_bit(UTIMER_COMPARE_CTRL_B(reg_base),
-					COMPARE_CTRL_DRV_DISABLE_VAL_HIGH_BIT);
-	}
-}
-
-static void utimer_update_pulse(uint32_t reg_base, uint8_t driver, uint32_t pulse_value)
-{
-	if (driver & ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) {
-		sys_write32(pulse_value, UTIMER_COMPARE_A(reg_base));
-	}
-	if (driver & ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE) {
-		sys_write32(pulse_value, UTIMER_COMPARE_B(reg_base));
-	}
+	return config_val;
 }
 
 static int pwm_alif_utimer_set_cycles(const struct device *dev, uint32_t channel,
@@ -137,55 +90,64 @@ static int pwm_alif_utimer_set_cycles(const struct device *dev, uint32_t channel
 	uint32_t global_base = cfg->global_base;
 	uint32_t value;
 
-	ARG_UNUSED(channel);
+	if (channel > NUM_CHANNELS) {
+		LOG_ERR("Invalid channel number");
+		return -EINVAL;
+	}
+
+	value = utimer_get_driver_config(cfg->counterdirection, flags);
+
+	/* disable driver output if period is zero */
+	if (period_cycles == 0U) {
+		alif_utimer_disable_driver(timer_base, channel);
+		alif_utimer_set_driver_disable_val_low(timer_base, channel);
+		if (flags == PWM_POLARITY_INVERTED) {
+			/* make driver disable state as high */
+			alif_utimer_set_driver_disable_val_high(timer_base, channel);
+		}
+
+		data->prev_period = 0;
+		return 0;
+	}
 
 	if (flags != data->prev_flags) {
-		value = (flags & PWM_POLARITY_INVERTED) ?
-			(COMPARE_CTRL_DRV_HIGH_AT_COMP_MATCH | COMPARE_CTRL_DRV_LOW_AT_CYCLE_END) :
-			(COMPARE_CTRL_DRV_LOW_AT_COMP_MATCH | COMPARE_CTRL_DRV_HIGH_AT_CYCLE_END);
-		utimer_config_driver(timer_base, cfg->driver_type, value);
+		value = utimer_get_driver_config(cfg->counterdirection, flags);
+		alif_utimer_config_driver_output(timer_base, channel, value);
 
 		data->prev_flags = flags;
 	}
 
 	/* update period value if it as been changed */
 	if (period_cycles != data->prev_period) {
-		/* config driver output as zero if period is zero */
-		if (period_cycles == 0U) {
-			utimer_compare_disable_driver(timer_base, cfg->driver_type,
-					data->prev_flags);
-		} else {
-			/* update the period value if it has changed and is not zero. */
-			if (data->prev_period == 0U) {
-				/* enable driver as previously it is disabled */
-				utimer_compare_enable_driver(timer_base, cfg->driver_type);
-			}
-			/* set period value */
-			sys_write32(period_cycles, UTIMER_CNTR_PTR(timer_base));
-		}
+		/* set period value */
+		alif_utimer_set_counter_reload_value(timer_base, period_cycles);
+
 		data->prev_period = period_cycles;
 	}
 
 	/* update pulse value if it as been changed */
 	if (pulse_cycles != data->prev_pulse) {
-		/* config driver output as zero if pulse is zero */
-		if (pulse_cycles == 0U) {
-			utimer_compare_disable_driver(timer_base, cfg->driver_type,
-					data->prev_flags);
-		} else {
-			if (data->prev_pulse == 0U) {
-				/* enable driver as previously it is disabled */
-				utimer_compare_enable_driver(timer_base, cfg->driver_type);
-			}
-			/* set pulse value */
-			utimer_update_pulse(timer_base, cfg->driver_type, pulse_cycles);
-		}
+		/* set pulse value */
+		alif_utimer_set_compare_value(timer_base, channel, pulse_cycles);
+
 		data->prev_pulse = pulse_cycles;
 	}
 
-	/* start the counter it is not already running */
-	if (!(sys_test_bit(UTIMER_GLB_CNTR_RUNNING(global_base), cfg->timer))) {
-		sys_set_bit(UTIMER_GLB_CNTR_START(global_base), cfg->timer);
+	/* enable channel if not enabled */
+	if (!alif_utimer_driver_enabled(timer_base, channel)) {
+		alif_utimer_config_driver_output(timer_base, channel, value);
+		alif_utimer_enable_driver_output(global_base, channel, cfg->timer_id);
+		alif_utimer_enable_driver(timer_base, channel);
+	}
+
+	/* enable channel compare match if not enabled */
+	if (!alif_utimer_comp_match_enabled(timer_base, channel)) {
+		alif_utimer_enable_compare_match(timer_base, channel);
+	}
+
+	/* start the counter not started */
+	if (!alif_utimer_counter_running(global_base, cfg->timer_id)) {
+		alif_utimer_start_counter(global_base, cfg->timer_id);
 	}
 
 	return 0;
@@ -194,11 +156,11 @@ static int pwm_alif_utimer_set_cycles(const struct device *dev, uint32_t channel
 static int pwm_alif_utimer_get_cycles_per_sec(const struct device *dev, uint32_t channel,
 				 uint64_t *cycles)
 {
-	const struct pwm_alif_utimer_config *cfg = dev->config;
+	struct pwm_alif_utimer_data *data = dev->data;
 
 	ARG_UNUSED(channel);
 
-	*cycles = cfg->frequency;
+	*cycles = data->frequency;
 	return 0;
 }
 
@@ -214,7 +176,6 @@ static int pwm_alif_utimer_init(const struct device *dev)
 	uint32_t timer_base = cfg->timer_base;
 	uint32_t global_base = cfg->global_base;
 	int32_t ret;
-	uint32_t reg;
 
 	/* apply pin configuration */
 	ret = pinctrl_apply_state(cfg->pcfg, PINCTRL_STATE_DEFAULT);
@@ -222,36 +183,34 @@ static int pwm_alif_utimer_init(const struct device *dev)
 		return ret;
 	}
 
-	if ((cfg->driver_type < ALIF_UTIMER_DRIVER_A_OUTPUT_ENABLE) ||
-			(cfg->driver_type > ALIF_UTIMER_DRIVER_B_OUTPUT_ENABLE)) {
-		LOG_ERR("ERROR: Invalid output driver");
+	/* get clock rate from clock manager */
+	if (!device_is_ready(cfg->clk_dev)) {
+		LOG_ERR("clock controller device not ready");
+		return -ENODEV;
+	}
+	ret = clock_control_get_rate(cfg->clk_dev,
+			cfg->clkid, &data->frequency);
+	if (ret != 0) {
+		LOG_ERR("Unable to get clock rate: err:%d", ret);
+		return ret;
 	}
 
+	/* disable for timer outputs */
+	alif_utimer_disable_timer_output(global_base, cfg->timer_id);
+
 	/* enable timer clock */
-	sys_set_bit(UTIMER_GLB_CLOCK_ENABLE(global_base), cfg->timer);
+	alif_utimer_enable_timer_clock(global_base, cfg->timer_id);
 
 	/* program enable for start, stop and clear counter */
-	sys_set_bit(UTIMER_START_1_SRC(timer_base), CNTR_SRC1_PGM_EN_BIT);
-	sys_set_bit(UTIMER_STOP_1_SRC(timer_base), CNTR_SRC1_PGM_EN_BIT);
-	sys_set_bit(UTIMER_CLEAR_1_SRC(timer_base), CNTR_SRC1_PGM_EN_BIT);
-
-	/* disable all buffer operations */
-	sys_write32(0x0, UTIMER_BUF_OP_CTRL(timer_base));
+	alif_utimer_enable_soft_counter_ctrl(cfg->timer_base);
 
 	/* set counter direction */
 	utimer_set_direction(timer_base, cfg->counterdirection);
 
-	/* enable driver output and default driver settings */
-	reg = (COMPARE_CTRL_DRV_COMPARE_EN | COMPARE_CTRL_DRV_DRIVER_EN |
-		   (COMPARE_CTRL_DRV_LOW_AT_COMP_MATCH & COMPARE_CTRL_DRV_COMP_MATCH_Msk) |
-		   (COMPARE_CTRL_DRV_HIGH_AT_CYCLE_END & COMPARE_CTRL_DRV_CYCLE_END_Msk));
-
-	utimer_config_driver(timer_base, cfg->driver_type, reg);
-	utimer_enable_output(global_base, cfg->driver_type, cfg->timer);
-	data->prev_flags = PWM_POLARITY_NORMAL;
-
 	/* enable timer counter */
-	sys_set_bit(UTIMER_CNTR_CTRL(timer_base), CNTR_CTRL_EN_BIT);
+	alif_utimer_enable_counter(timer_base);
+
+	data->prev_flags = PWM_POLARITY_NORMAL;
 
 	return 0;
 }
@@ -263,21 +222,20 @@ static int pwm_alif_utimer_init(const struct device *dev)
 	static const struct pwm_alif_utimer_config pwm_alif_utimer_cfg_##n = {			\
 		.global_base = (uint32_t) DT_REG_ADDR_BY_NAME(DT_INST_PARENT(n), global),	\
 		.timer_base = (uint32_t) DT_REG_ADDR_BY_NAME(DT_INST_PARENT(n), timer),		\
-		.frequency = DT_PROP(DT_INST_PARENT(n), clock_frequency),			\
 		.counterdirection = DT_PROP(DT_INST_PARENT(n), counter_direction),		\
-		.driver_type = DT_PROP(DT_INST_PARENT(n), driver_enable),			\
-		.timer = DT_PROP(DT_INST_PARENT(n), timer_id),					\
+		.timer_id = DT_PROP(DT_INST_PARENT(n), timer_id),				\
 		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),					\
+		.clk_dev = DEVICE_DT_GET(DT_CLOCKS_CTLR(DT_INST_PARENT(n))),			\
+		.clkid = (clock_control_subsys_t)DT_CLOCKS_CELL(DT_INST_PARENT(n), clkid),	\
 	};											\
 												\
 	DEVICE_DT_INST_DEFINE(n,								\
-				&pwm_alif_utimer_init,						\
-				NULL,								\
-				&pwm_alif_utimer_data_##n,					\
-				&pwm_alif_utimer_cfg_##n,					\
-				POST_KERNEL,							\
-				CONFIG_PWM_INIT_PRIORITY,					\
-				&pwm_alif_utimer_driver_api);
+			      &pwm_alif_utimer_init,						\
+			      NULL,								\
+			      &pwm_alif_utimer_data_##n,					\
+			      &pwm_alif_utimer_cfg_##n,						\
+			      POST_KERNEL,							\
+			      CONFIG_PWM_INIT_PRIORITY,						\
+			      &pwm_alif_utimer_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(PWM_ALIF_UTIMER_INIT)
-
